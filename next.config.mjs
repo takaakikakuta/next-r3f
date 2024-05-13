c9facb1509ce7871201ba27b5ea95ab18dbf4af6
takaaki <soci0926@gmail.com>
2024-05-13 17:38:28 +0900

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['museums320240303.s3.amazonaws.com'],
+    },
+  };
+  
+  export default nextConfig;
